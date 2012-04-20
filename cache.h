cd49d48f42febbc5650789c743b03e0c0439d70e
Sterling M. Alexander <sterlingalexander@gmail.com>
2012-04-20 09:17:06 -0400

    typo

--- cache.h
+++ cache.h
@@ -86,7 +86,7 @@ public:
 class Cache {
 protected:
     ulong size, lineSize, assoc, sets, log2Sets, log2Blk, tagMask, numLines;
-    ulong reads, readMisses, writes, writeMisses, writeBacks invalidations;
+    ulong reads, readMisses, writes, writeMisses, writeBacks, invalidations;
     ulong memoryTransactions, cacheToCacheTransfers;
     ulong protocol;
 
